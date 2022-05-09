2b6c2291a4eafa8e958fbc62d3598208c14afc36
SerchPro <proyects_serch@hotmail.com>
2022-05-09 18:55:09 -0500

    adding emit to socket

--- models/sockets.js
+++ models/sockets.js
@@ -1,4 +1,4 @@
-const Markers = require("./Markers");
+const Markers = require("./markers");
 
 
 class Sockets {
@@ -15,10 +15,16 @@ class Sockets {
         this.io.on('connection', ( socket ) => {
 
             socket.emit('active-markers', this.markers.actives);
+
             socket.on('new-marker', (marker) =>{
-                console.log("new marker", marker)
+                //console.log("new marker", marker)
                 this.markers.add(marker)
-                socket.broadcast('new-marker', marker)
+                socket.broadcast.emit('new-marker', marker)
+            });
+
+            socket.on('update-marker', (marker) =>{
+                this.markers.update(marker);
+                socket.broadcast.emit('update-marker', marker);
             })
 
         });
